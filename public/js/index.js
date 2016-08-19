bab2e1c9550538cc475bd71378471c64327af1e7
zhou <jinzita007@yahoo.com>
2016-08-19 17:55:48 +0800

    0.0.5

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,15 @@
-$(function(){
-  var tmp = null,
-    $title = $('.title'),
-    $con = $('.title > ul,.title > ul > li');
+$(document).ready(function() {
+  $('.nav_list_a').click(function() {
+  //var tmp = null,
+    //$title = $('.nav li'),
+   // $con = $('.nav li > ul');
  
-  $title.click(function(){
-    $(tmp).children('ul').slideUp().end().children('.arrow').removeClass('arrow-up');
-    $(this).children('ul').slideToggle().end().children('.arrow').addClass('arrow-up');
-    tmp = this;
+ // $title.click(function(){
+    $('.nav li').children('ul').slideUp().end().children('.arrow').removeClass('arrow-up');
+    $('.nav li > ul').children('ul').slideToggle().end().children('.arrow').addClass('arrow-up');
+    //tmp = this;
   });
-  $con.click(function(){
-    return false;  // 阻止事件冒泡
-  });
+ // $con.click(function(){
+ //   return false;  // 阻止事件冒泡
+//  });
 });
